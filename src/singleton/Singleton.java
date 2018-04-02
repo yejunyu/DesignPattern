d5db93df7c883f82bea7a4982af45d84365450b9
yejunyu <liuxing_yue@126.com>
2018-04-02 23:37:58 +0800

    单例

--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -6,6 +6,12 @@ import java.io.Serializable;
  * @author yejunyu
  * @date 2018/4/2.
  */
+
+/**
+ * 序列化的源码
+ * obj = desc.isInstantiable() ? desc.newInstance() : null;
+ * 序列化是通过反射调用无参构造器创建一个新的对象
+ */
 public class Singleton implements Serializable{
     private volatile static Singleton instance;
     private Singleton(){}
