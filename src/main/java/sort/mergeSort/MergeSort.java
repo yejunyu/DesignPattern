e81caf9906d01765ce510e51fa411f7ca07e34ef
yejunyu <liuxing_yue@126.com>
2019-08-14 09:16:15 +0800

    mergesort

---
+++ src/main/java/sort/mergeSort/MergeSort.java
@@ -0,0 +1,21 @@
+package sort.mergeSort;
+
+import sort.CallBack;
+
+/**
+ * @author: YeJunyu
+ * @description:
+ * @email: yyyejunyu@gmail.com
+ * @date: 2019/8/14
+ */
+public class MergeSort implements CallBack {
+    public MergeSort() {
+        System.out.println("归并排序");
+    }
+
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] ts) {
+
+        return null;
+    }
+}
