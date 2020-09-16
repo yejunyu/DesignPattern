303b8d3416434c2c2f2a1978bb25d8fbc562bdd8
yejunyu <liuxing_yue@126.com>
2020-09-16 09:25:51 +0800

    单例

--- src/main/java/designPattern/singleton/SingletonEnum.java
+++ src/main/java/designPattern/singleton/SingletonEnum.java
@@ -4,10 +4,22 @@ package designPattern.singleton;
  * @author yejunyu
  * @date 2018/4/2.
  */
-public enum SingletonEnum {
-    INSTANCE;
+public class SingletonEnum {
 
-    SingletonEnum(){
+    private SingletonEnum() {
+    }
 
+    private enum singleton {
+        INSTANCE;
+
+        private final SingletonEnum instance;
+
+        singleton() {
+            instance = new SingletonEnum();
+        }
+    }
+
+    public SingletonEnum getInstance() {
+        return singleton.INSTANCE.instance;
     }
 }
